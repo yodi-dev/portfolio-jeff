e577e1dc02f02b6d3d7f74c4d24c41525feb12a0
irawanyodi <dykhazta@gmail.com>
2025-08-02 16:29:57 +0700

    add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxt/eslint"],
+	modules: ["@nuxt/eslint", '@nuxt/devtools'],
 	devtools: { enabled: true },
+	ssr: true,
 	compatibilityDate: "2024-11-01",
   css: [
 	"@picocss/pico",
 	'@/assets/css/main.css'
 ],
+postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
 	eslint: {
 		config: {
 			stylistic: {
